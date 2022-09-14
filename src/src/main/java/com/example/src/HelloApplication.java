b8a9dbdface5185cb3b5ccd14e2b4cc349ae4c44
Christianne1 <imaene@iu.edu>
2022-09-14 00:58:44 -0400

    The Disco Bot Prototype
    The goal of this commit is to wrap up Sprint 1 with the finished prototye.
    
    The reasons why this commit makes a good prototype are:
    - Maven is running as our build system
    - There is a unit test running in src
    - Javadoc is in use for documentation
    - Intellij IDEA has a code linter built in.

--- src/src/main/java/com/example/src/HelloApplication.java
+++ src/src/main/java/com/example/src/HelloApplication.java
@@ -7,16 +7,27 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * This is the main class called HelloApplication
+ */
 public class HelloApplication extends Application {
+    /**
+     * This function sets the stage and calls other functions to produce effects
+     * @param stage is a variable of the Stage data type.
+     * @throws IOException
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Disco");
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * @param args This is the main function, and it is a standard function used to run code.
+     */
     public static void main(String[] args) {
         launch();
     }
--- src/src/main/java/com/example/src/HelloController.java
+++ src/src/main/java/com/example/src/HelloController.java
@@ -3,12 +3,18 @@ package com.example.src;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+/**
+ * This is a class called HelloController
+ */
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+    /**
+     * This is a void method that drives the button on the popup application.
+     */
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("What's your name?");
     }
 }
