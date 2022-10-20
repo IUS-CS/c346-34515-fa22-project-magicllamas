3584101b0eab1ddcfb995ac068e7c30da9099ec5
Christianne1 <imaene@iu.edu>
2022-10-20 14:11:18 -0400

    This is the test for the bot to ensure that he can ignore bot messages.

--- src/test/java/responder/prototype/listener/InsultGeneratorTest.java
+++ src/test/java/responder/prototype/listener/InsultGeneratorTest.java
@@ -1,18 +1,29 @@
 package responder.prototype.listener;
-
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
+class InsultGeneratorTest{
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class InsultGeneratorTest {
     @Test
+    public void botIgnoredTest() { //wait until a message is sent on discord
 
-    void test() {
+        MessageReceivedEvent event = mock(MessageReceivedEvent.class); //mocking the event object
+        User user = mock(User.class); // mocking the user object
+        when(user.isBot()).thenReturn(true); // setting up what each method should return
+        when(event.getAuthor()).thenReturn(user);
 
-        System.out.println("Bot is currently tested on Discord channel");
+        //ignore bot messages?
+        //do not respond to other bots
+        if (event.getAuthor().isBot()) {
+            assertTrue(event.getAuthor().isBot());
+            //
+        }
     }
 
 }
