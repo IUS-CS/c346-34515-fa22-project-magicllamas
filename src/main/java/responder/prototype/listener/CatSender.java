92778140a92252e76aee47311fce8e2d59b6d37a
Louis Wiley <louiswiley474@gmail.com>
2022-10-27 17:54:47 -0400

    edited CatSender

--- src/main/java/responder/prototype/listener/CatSender.java
+++ src/main/java/responder/prototype/listener/CatSender.java
@@ -11,8 +11,9 @@ public class CatSender extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
     if(event.getName().equals("CatLauncher")){
-        event.reply("https://purepng.com/public/uploads/large/purepng.com-catcat-animal-981524644802hvred.png");
+        event.reply("https://purepng.com/public/uploads/large/purepng.com-catcat-animal-981524644802hvred.png").queue();
 
     }
+
     }
 }
