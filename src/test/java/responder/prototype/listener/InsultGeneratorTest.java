f4011ccf736c0fdbc3077e07952951b71073f762
bramora <98437411+bramora@users.noreply.github.com>
2022-11-07 22:10:51 -0500

    working MessageReceivedEvent mock made

--- src/test/java/responder/prototype/listener/InsultGeneratorTest.java
+++ src/test/java/responder/prototype/listener/InsultGeneratorTest.java
@@ -1,38 +1,52 @@
 package responder.prototype.listener;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
+import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.when;
+
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class InsultGeneratorTest {
-    private InsultGenerator insultGenerator;  // insult generator constructor
+    private InsultGenerator insultGenerator;
 
-    private MessageReceivedEvent botIgnoredEvent() { //method allows me to mock objects and test our version of the code.
+    /*private MessageReceivedEvent scEvent() {
         MessageReceivedEvent event = mock(MessageReceivedEvent.class);
+        Message getMessage= mock(Message.class);
         User user = mock(User.class); // mocking the user object
         when(user.isBot()).thenReturn(true); // setting up what each method should return
         when(event.getAuthor()).thenReturn(user);
+        when(event.getMessage()).thenReturn(getMessage);
 
 
         return event;
     }
     @Test
-    public void botIgnoredTest() { //can tell if user with event is bot
+    public void botIgnoredTest() { //can tell if person with event is bot
         insultGenerator = new InsultGenerator();
-        var event = botIgnoredEvent();
+        var event = scEvent();
         insultGenerator.onMessageReceived(event);
 
-    }
+    }*/
 
+   // @Test
+   // public void onExclamation() { //Does the bot respond with an exclamation mark
+       // insultGenerator = new InsultGenerator();
+       // var event =scEvent();
+       // insultGenerator.onMessageReceived(event);
+   // }
 
-    private MessageReceivedEvent oldSaltEvent() {//method allows me to mock objects and test our version of the code.
+    private MessageReceivedEvent oldSaltEvent() {
         MessageReceivedEvent event = mock(MessageReceivedEvent.class);
         Message getMessage= mock(Message.class);
         User user = mock(User.class); // mocking the user object
@@ -47,98 +61,103 @@ class InsultGeneratorTest {
         return event;
     }
     @Test
-    public void onExclamation () { //Does the bot respond with an exclamation mark
+    public void onTry () { //Does the bot respond with an exclamation mark
 
         insultGenerator = new InsultGenerator();
         var event = oldSaltEvent();
         insultGenerator.onMessageReceived(event);
     }
-    private MessageReceivedEvent crustyExplorerEvent() {//method allows me to mock objects and test our version of the code.
-        MessageReceivedEvent event = mock(MessageReceivedEvent.class);
-        Message getMessage= mock(Message.class);
-        User user = mock(User.class); // mocking the user object
-        MessageChannelUnion channel = mock(MessageChannelUnion.class);
-        MessageCreateAction callback = mock(MessageCreateAction.class);
-        when(event.getAuthor()).thenReturn(user);
-        when(event.getMessage()).thenReturn(getMessage);
-        when(getMessage.getContentRaw()).thenReturn("try");
-        when(event.getChannel()).thenReturn(channel);
-        when(channel.sendMessage("You Crusty Nut Explorer!")).thenReturn(callback);
 
-        return event;
-    }
     @Test
-    public void onTry () { //Does the bot respond with channel.sendMessage(...)
+    public void onPull () { //Does the bot respond with an exclamation mark
 
-        insultGenerator = new InsultGenerator();
-        var event = crustyExplorerEvent();
-        insultGenerator.onMessageReceived(event);
-    }
-
-    private MessageReceivedEvent weinerWizardEvent() {//method allows me to mock objects and test our version of the code.
-        MessageReceivedEvent event = mock(MessageReceivedEvent.class);
-        Message getMessage= mock(Message.class);
+        MessageReceivedEvent event = mock(MessageReceivedEvent.class); //mocking the event object
+        MessageChannel channel = mock(MessageChannel.class); //mocking the channel object
         User user = mock(User.class); // mocking the user object
-        MessageChannelUnion channel = mock(MessageChannelUnion.class);
-        MessageCreateAction callback = mock(MessageCreateAction.class);
+        when(user.isBot()).thenReturn(true); // setting up what each method should return
         when(event.getAuthor()).thenReturn(user);
-        when(event.getMessage()).thenReturn(getMessage);
-        when(getMessage.getContentRaw()).thenReturn("pull");
-        when(event.getChannel()).thenReturn(channel);
-        when(channel.sendMessage("Pull harder, You Silly Wiener Wizard!")).thenReturn(callback);
 
-        return event;
+        //acknowledge user messages and print them to console
+        while (!(event.getAuthor().isBot())) {
+            try {
+                wait();
+                String content = event.getMessage().getContentRaw();
+                Boolean botsaid = true;
+
+                if (content.contains("pull")) { //if user sends a "pull"
+                    String botMessage = channel.sendMessage("Pull harder, You Silly Wiener Wizard!").getContent(); //this is bot response
+                    if (!botMessage.isEmpty()) {
+                        assertTrue(botsaid);
+                    }
+                }
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Thread Interrupted");
+            }
+
+        }
     }
+
     @Test
-    public void onPull () { //Does the bot respond with channel.sendMessage(...)
+    public void onMd () { //Does the bot respond with an exclamation mark
 
-        insultGenerator = new InsultGenerator();
-        var event = weinerWizardEvent();
-        insultGenerator.onMessageReceived(event);
-    }
-    private MessageReceivedEvent nutDoctorEvent() {//method allows me to mock objects and test our version of the code.
-        MessageReceivedEvent event = mock(MessageReceivedEvent.class);
-        Message getMessage= mock(Message.class);
+        MessageReceivedEvent event = mock(MessageReceivedEvent.class); //mocking the event object
+        MessageChannel channel = mock(MessageChannel.class); //mocking the channel object
         User user = mock(User.class); // mocking the user object
-        MessageChannelUnion channel = mock(MessageChannelUnion.class);
-        MessageCreateAction callback = mock(MessageCreateAction.class);
+        when(user.isBot()).thenReturn(true); // setting up what each method should return
         when(event.getAuthor()).thenReturn(user);
-        when(event.getMessage()).thenReturn(getMessage);
-        when(getMessage.getContentRaw()).thenReturn("md");
-        when(event.getChannel()).thenReturn(channel);
-        when(channel.sendMessage("Ok nut doctor!")).thenReturn(callback);
 
-        return event;
+        //acknowledge user messages and print them to console
+        while (!(event.getAuthor().isBot())) {
+            try {
+                wait();
+                String content = event.getMessage().getContentRaw();
+                Boolean botsaid = true;
+
+                if (content.contains("md")) { //if user sends an "md"
+                    String botMessage = channel.sendMessage("Ok nut doctor!").getContent(); //this is bot response
+                    if (!botMessage.isEmpty()) {
+                        assertTrue(botsaid);
+                    }
+                }
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Thread Interrupted");
+            }
+
+        }
     }
+
     @Test
-    public void onMd () {//Does the bot respond with channel.sendMessage(...)
+    public void onRequest () { //Does the bot respond with an exclamation mark
 
-        insultGenerator = new InsultGenerator();
-        var event = nutDoctorEvent();
-        insultGenerator.onMessageReceived(event);
-    }
-
-    private MessageReceivedEvent mergeRequest() {//method allows me to mock objects and test our version of the code.
-        MessageReceivedEvent event = mock(MessageReceivedEvent.class);
-        Message getMessage= mock(Message.class);
+        MessageReceivedEvent event = mock(MessageReceivedEvent.class); //mocking the event object
+        MessageChannel channel = mock(MessageChannel.class); //mocking the channel object
         User user = mock(User.class); // mocking the user object
-        MessageChannelUnion channel = mock(MessageChannelUnion.class);
-        MessageCreateAction callback = mock(MessageCreateAction.class);
+        when(user.isBot()).thenReturn(true); // setting up what each method should return
         when(event.getAuthor()).thenReturn(user);
-        when(event.getMessage()).thenReturn(getMessage);
-        when(getMessage.getContentRaw()).thenReturn("request");
-        when(event.getChannel()).thenReturn(channel);
-        when(channel.sendMessage("You are a request and I will not merge you")).thenReturn(callback);
 
-        return event;
+        //acknowledge user messages and print them to console
+        while (!(event.getAuthor().isBot())) {
+            try {
+                wait();
+                String content = event.getMessage().getContentRaw();
+                Boolean botsaid = true;
+
+                if (content.contains("request")) { //if user sends a "request"
+                    String botMessage = channel.sendMessage("You are a request and I will not merge you").getContent(); //this is bot response
+                    if (!botMessage.isEmpty()) {
+                        assertTrue(botsaid);
+                    }
+                }
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Thread Interrupted");
+            }
+
+        }
     }
-    @Test
-    public void onRequest () {//Does the bot respond with channel.sendMessage(...)
-
-        insultGenerator = new InsultGenerator();
-        var event = mergeRequest();
-        insultGenerator.onMessageReceived(event);
-    }
-
-
 }
