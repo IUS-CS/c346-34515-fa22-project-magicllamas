0d7371d206fde233d113373d8d5bdb99f4383e32
Louis Wiley <louiswiley474@gmail.com>
2022-11-10 18:27:09 -0500

    Fixed my IDE and Git and added my cat launcher

--- src/main/java/responder/prototype/listener/CatSender.java
+++ src/main/java/responder/prototype/listener/CatSender.java
@@ -1,19 +1,34 @@
 package responder.prototype.listener;
 
 
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CatSender extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-    if(event.getName().equals("CatLauncher")){
-        event.reply("https://purepng.com/public/uploads/large/purepng.com-catcat-animal-981524644802hvred.png").queue();
-
+        String command = event.getName();
+        String content = "Meow";
+        if (command.equals("Cat Launcher")) {
+            ReplyCallbackAction action = event.reply(content);
+            action.queue();
+        }
     }
 
+    @Override
+    public void onGuildReady(@NotNull GuildReadyEvent event) {
+        List<CommandData> commandData = new ArrayList<>();
+        commandData.add(Commands.slash("Cat Launcher", "Launch a cat at friends"));
+        event.getGuild().updateCommands().addCommands(commandData).queue();
     }
 }
